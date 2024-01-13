16c148d3cfd50d081958dc18676f9d2b86eef431
LOctavioDev <octaviodevtech@gmail.com>
2024-01-13 17:58:26 -0600

    Add: CRUD API

--- src/routes/user.py
+++ src/routes/user.py
@@ -1,28 +1,38 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Response, status
 from ..config.db import conn
 from ..schemas.user import userEntity, usersEntity
 from ..models.user import User
+from passlib.hash import sha256_crypt
+from bson import ObjectId
+from starlette.status import HTTP_204_NO_CONTENT
 
 user = APIRouter()
 
 @user.get('/users')
 def find_all_users():
-    return usersEntity(conn.local.user.find()) 
-
+    return usersEntity(conn.groceriesdb.user.find()) 
+ 
 @user.post('/users')
 def create_user(user: User):
     new_user = dict(user)
-    print(new_user)
-    return "recived"
+    new_user["password"] = sha256_crypt.encrypt(new_user["password"])
+    del new_user["id"]
+    
+    id = conn.groceriesdb.user.insert_one(new_user).inserted_id
+    user = conn.groceriesdb.user.find_one({"_id": id})
 
-@user.get('/users/{id}')
-def find_users():
-    return 'hello world'
+    return userEntity(user)
+
+@user.get('/user/{id}')
+def find_user(id: str):
+    return userEntity(conn.groceriesdb.find_one({"_id": ObjectId(id)}))
 
 @user.put('/user/{id}')
-def update_user():
-    return 'hello world'
+def update_user(id: str, user: User):
+    conn.groceriesdb.find_one_and_update({"_id": ObjectId(id)},{"$set": dict(user)})
 
 @user.delete('/user/{id}')
-def delete_user():
-    return 'hello world'
+def delete_user(id: str):
+    userEntity(conn.groceriesdb.user.find_one_and_delete({"_id": ObjectId(id)}))   
+    return Response(status_code=HTTP_204_NO_CONTENT)
+
