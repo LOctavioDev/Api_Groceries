5be5ee41527140335a6a8c3f9307bc4a7a0d7ae3
LOctavioDev <octaviodevtech@gmail.com>
2024-01-13 13:54:36 -0600

    first commit

---
+++ main.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+users = [
+    {
+        'id': 1,
+        'age': '20',
+        'name': 'Octavio',
+        'last_name': 'DEV'
+    },
+    {
+        'id': 2,
+        'age': '19',
+        'name': 'Octa',
+        'last_name': 'LM'
+    }
+]
+
+@app.get('/')
+def message():
+    return 'Hello world'
+    
+@app.get('/users')
+def get_users():
+    return users
+    
+@app.get('/user/{id}')
+def get_user(id):
+    for user in users:
+        if user['id'] == int(id):
+            return user
+        
+    return "user not found"
